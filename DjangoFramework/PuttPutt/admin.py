673d0d637f9dd06f7ed542170d3c6984fde2314d
phat_sumo <ethanjpayne1@gmail.com>
2021-10-29 01:32:07 -0600

    add Profile to admin page

--- DjangoFramework/PuttPutt/admin.py
+++ DjangoFramework/PuttPutt/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from PuttPutt.models import Drink
+from PuttPutt.models import Profile
 
 # Register your models here.
 admin.site.register(Drink)
+admin.site.register(Profile)
